ebd78dfc7f8f11f363488563c20534e684e61477
任丕明 <renpiming@chtwm.com>
2021-11-05 15:36:38 +0800

    添加mongodb 的存储方法！

--- wechat/src/main/java/com/rpm/wework/chatrecord/DownLoadChatData.java
+++ wechat/src/main/java/com/rpm/wework/chatrecord/DownLoadChatData.java
@@ -1,18 +1,21 @@
 package com.rpm.wework.chatrecord;
 
 import com.alibaba.fastjson.JSON;
+import com.rpm.wework.MessageService;
 import com.rpm.wework.config.WeWorkProperties;
 import com.tencent.wework.Finance;
 import com.tencent.wework.RSAEncrypt;
 import com.tencent.wework.domain.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Slf4j
@@ -24,11 +27,14 @@ public class DownLoadChatData {
     WeWorkProperties weWorkProperties;
     @Autowired
     StringRedisTemplate template;
+    @Autowired
+    @Qualifier("messageServiceMongo")
+    MessageService messageService;
     private long limit;
     private long timeout;
     private String proxy;
     private String passwd;
-    private static final String WEWORK_LAST_SEQ="WEWORK:LAST:SEQ";
+    private static final String WEWORK_LAST_SEQ = "WEWORK:LAST:SEQ";
 
     @PostConstruct
     private void init() {
@@ -36,7 +42,7 @@ public class DownLoadChatData {
         this.timeout = weWorkProperties.getTimeout() == 0 ? 50 : weWorkProperties.getTimeout();
         this.proxy = weWorkProperties.getProxy();
         this.passwd = weWorkProperties.getPasswd();
-        lastSeq = template.opsForValue().increment(WEWORK_LAST_SEQ,0);
+        lastSeq = template.opsForValue().increment(WEWORK_LAST_SEQ, 0);
         this.sdk = Finance.NewSdk();
         long ret = 0;
         ret = Finance.Init(sdk, weWorkProperties.getCorpid(), weWorkProperties.getSecret());
@@ -82,6 +88,7 @@ public class DownLoadChatData {
         if (errcode == 0) {
             ChatData[] chatdata = chatDatas.getChatdata();
             log.info("Finance.GetContentFromSlice msgsize:{}", chatdata.length);
+            List<Message> messages  = new ArrayList<>();
             for (int i = 0; i < chatdata.length; i++) {
                 //第三步 解析密文到明文
                 ChatData data = chatdata[i];
@@ -104,7 +111,7 @@ public class DownLoadChatData {
                 if (msgType != null) {
                     switch (msgType) {
                         case TEXT:
-                            Text text = message.getText();
+                            messages.add(message);
                             break;
                         case IMAGE:
                             Image image = message.getImage();
@@ -112,6 +119,7 @@ public class DownLoadChatData {
                             map.put("sdkFileId", image.getSdkfileid());
                             map.put("fileName", message.getMsgid() + ".jpg");
                             MediaDataQueue.INSTANCE.add(map);
+                            messages.add(message);
                             break;
                         case FILE:
                             com.tencent.wework.domain.File file = message.getFile();
@@ -119,13 +127,17 @@ public class DownLoadChatData {
                             map.put("sdkFileId", file.getSdkfileid());
                             map.put("fileName", file.getFilename());
                             MediaDataQueue.INSTANCE.add(map);
+                            messages.add(message);
                             break;
                         default:
                     }
                 } else {
                     //如果没有msgType 说明是切换企业日志
                 }
-                template.opsForValue().set(WEWORK_LAST_SEQ,lastSeq+"");
+
+                template.opsForValue().set(WEWORK_LAST_SEQ, lastSeq + "");
+                //存储进去mongodb
+                messageService.saveAll(messages);
             }
         }
     }
--- wechat/src/main/java/com/rpm/wework/chatrecord/MediaDataHandler.java
+++ wechat/src/main/java/com/rpm/wework/chatrecord/MediaDataHandler.java
@@ -1,12 +1,15 @@
 package com.rpm.wework.chatrecord;
 
+import com.rpm.wework.MessageService;
 import com.rpm.wework.config.WeWorkProperties;
 import com.tencent.wework.Finance;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Map;
 import java.util.concurrent.ThreadPoolExecutor;
 
@@ -18,6 +21,9 @@ public class MediaDataHandler {
     ThreadPoolExecutor executor;
     @Autowired
     WeWorkProperties properties;
+    @Autowired
+    @Qualifier("messageServiceMongo")
+    MessageService messageService;
     /**
      * 处理队列
      */
@@ -67,39 +73,53 @@ public class MediaDataHandler {
         private void saveFile(Map<String, String> map) {
             String sdkFileId = map.get("sdkFileId");
             String FileName = map.get("fileName");
+            String msgid = map.get("msgid");
             String indexbuf = "";
             FileName = "D:\\data\\" + FileName;
+            FileOutputStream outputStream = null;
 
-            while (true) {
-                //每次使用GetMediaData拉取存档前需要调用NewMediaData获取一个media_data，在使用完media_data中数据后，还需要调用FreeMediaData释放。
-                long mediaData = Finance.NewMediaData();
-                int ret = Finance.GetMediaData(sdk, indexbuf, sdkFileId, properties.getProxy(), properties.getPasswd(), 30, mediaData);
-                if (ret != 0) {
-                    log.error("getmediadata ret{},map:{}",ret,map);
-                    Finance.FreeMediaData(mediaData);
-                    MediaDataQueue.INSTANCE.add(map);
-                    throw new RuntimeException("init sdk err ret:" + ret);
-                }
-                log.info("getmediadata outindex len:{}, data_len:{}, is_finis:{}", Finance.GetIndexLen(mediaData), Finance.GetDataLen(mediaData), Finance.IsMediaDataFinish(mediaData));
-                try {
+            try {
+                outputStream = new FileOutputStream(new java.io.File(FileName), true);
+                while (true) {
+                    //每次使用GetMediaData拉取存档前需要调用NewMediaData获取一个media_data，在使用完media_data中数据后，还需要调用FreeMediaData释放。
+                    long mediaData = Finance.NewMediaData();
+                    int ret = Finance.GetMediaData(sdk, indexbuf, sdkFileId, properties.getProxy(), properties.getPasswd(), 30, mediaData);
+                    if (ret != 0) {
+                        log.error("getmediadata ret{},map:{}", ret, map);
+                        Finance.FreeMediaData(mediaData);
+                        MediaDataQueue.INSTANCE.add(map);
+                        throw new RuntimeException("init sdk err ret:" + ret);
+                    }
+                    log.info("getmediadata outindex len:{}, data_len:{}, is_finis:{}", Finance.GetIndexLen(mediaData), Finance.GetDataLen(mediaData), Finance.IsMediaDataFinish(mediaData));
+
                     //大于512k的文件会分片拉取，此处需要使用追加写，避免后面的分片覆盖之前的数据。
-                    FileOutputStream outputStream = new FileOutputStream(new java.io.File(FileName), true);
-                    outputStream.write(Finance.GetData(mediaData));
-                    outputStream.close();
-                } catch (Exception e) {
-                    log.error("Save File Filed {}", e);
-                }
+                    // 此处使用存储文件的方法进行替换
 
-                if (Finance.IsMediaDataFinish(mediaData) == 1) {
-                    //已经拉取完成最后一个分片
-                    Finance.FreeMediaData(mediaData);
-                    break;
-                } else {
-                    //获取下次拉取需要使用的indexbuf
-                    indexbuf = Finance.GetOutIndexBuf(mediaData);
-                    Finance.FreeMediaData(mediaData);
+                    outputStream.write(Finance.GetData(mediaData));
+
+                    if (Finance.IsMediaDataFinish(mediaData) == 1) {
+                        //已经拉取完成最后一个分片
+                        Finance.FreeMediaData(mediaData);
+                        break;
+                    } else {
+                        //获取下次拉取需要使用的indexbuf
+                        indexbuf = Finance.GetOutIndexBuf(mediaData);
+                        Finance.FreeMediaData(mediaData);
+                    }
+                }
+            } catch (Exception e) {
+                log.error("Save File Filed {}", e);
+            }finally {
+                if (outputStream != null) {
+                    try {
+                        outputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
+            //更新图片
+            messageService.updateFileUrl(msgid, FileName);
         }
     }
 }
